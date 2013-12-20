f3c1535b868d01a1782541b49a61c7e3769c2a28
Ponnulakshmi <sponnulakshmi@yahoo.com>
2013-12-20 17:36:02 +0100

    initService

--- ScalableStorageService/src/app_kvEcs/ECSClient.java
+++ ScalableStorageService/src/app_kvEcs/ECSClient.java
@@ -20,76 +20,84 @@ import ecs.KVActions;
 import ecs.KVServerData;
 
 public class ECSClient {
-	
+
 	private static Logger logger = Logger.getRootLogger();
 	private static final String PROMPT = "ECS> ";
-	private String ecsConfig ;
+	private String ecsConfig;
 	private boolean stop = false;
-	List <KVServerData> p = new ArrayList<KVServerData>();
+	List<KVServerData> p = new ArrayList<KVServerData>();
 	private Md5 hash = new Md5();
-	KVActions kvActions = null ;
-	
-	public ECSClient(String filename){
-		this.ecsConfig=filename;
+	KVActions kvActions = null;
+
+	public ECSClient(String filename) {
+		this.ecsConfig = filename;
 	}
-	
+
 	public void run() {
-		
+
 		BufferedReader br = null;
-		BufferedReader stdin = null ;
+		BufferedReader stdin = null;
 		String sCurrentLine;
-		
-		
-			try {
-				br = new BufferedReader(new FileReader(ecsConfig));
-				while ((sCurrentLine = br.readLine()) != null) {
-					
-					String[] tokens = sCurrentLine.split("\\s+");
-					KVServerData temp = new KVServerData();
-					temp.setName(tokens[0]);
-					temp.setIp(tokens[1]);
-					temp.setPort(tokens[2]);
-					temp.setPosition(hash.hashIpPort(tokens[1],tokens[2]));
-					temp.setRunning(false);
-					temp.setAccepting(false);
-					p.add(temp);
-					logger.info("server info successfully inserted");
-				}
-					while(!stop) {
-						stdin = new BufferedReader(new InputStreamReader(System.in));
-						System.out.print(PROMPT);
-						
-						String cmdLine = stdin.readLine();
-						this.handleCommand(cmdLine);
-					}
-				}
-			 catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+
+		try {
+			br = new BufferedReader(new FileReader(ecsConfig));
+			while ((sCurrentLine = br.readLine()) != null) {
+
+				String[] tokens = sCurrentLine.split("\\s+");
+				KVServerData temp = new KVServerData();
+				temp.setName(tokens[0]);
+				temp.setIp(tokens[1]);
+				temp.setPort(tokens[2]);
+				temp.setPosition(hash.hashIpPort(tokens[1], tokens[2]));
+				temp.setRunning(false);
+				temp.setAccepting(false);
+				p.add(temp);
+				logger.info("server info successfully inserted");
 			}
-		
+			while (!stop) {
+				stdin = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print(PROMPT);
+
+				String cmdLine = stdin.readLine();
+				this.handleCommand(cmdLine);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
+
 	private void handleCommand(String cmdLine) {
-		
+
 		Random randomGenerator = new Random();
 		String[] tokens = cmdLine.split("\\s+");
-		
-		if(tokens[0].equals("initService")) {
-			if(tokens.length == 1) {
-				logger.info("Received " + tokens[0].toString());
-				int randomInt = randomGenerator.nextInt(p.size()) + 1 ;
+
+		if (tokens[0].equals("initService")) {
+			logger.info("Received " + tokens[0].toString());
+			int no_nodes = Integer.parseInt(tokens[1]);
+			for (int i = 1; i <= no_nodes; i++) {
+				int randomInt = randomGenerator.nextInt(p.size()) + 1;
 				kvActions = new KVActions(p);
 				try {
-					kvActions.initService(randomInt);
+					BufferedReader br = new BufferedReader(new FileReader(
+							"app_kvEcs/config.txt"));
+					String line = null;
+					int port = 0;
+					while ((line = br.readLine()) != null) {
+						if (line.contains("node" + randomInt)) {
+							port = Integer.parseInt(line.split("\\s+")[2]);
+						}
+					}
+					br.close();
+					kvActions.initService(randomInt, port);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		else if(tokens[0].equals("start")) {
-			if(tokens.length == 1) {
+		} else if (tokens[0].equals("start")) {
+			if (tokens.length == 1) {
 				logger.info("Received " + tokens[0].toString());
 				kvActions = new KVActions(p);
 				try {
@@ -97,12 +105,11 @@ public class ECSClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
-		}
-		else if(tokens[0].equals("stop")) {
-			if(tokens.length == 1) {
+
+		} else if (tokens[0].equals("stop")) {
+			if (tokens.length == 1) {
 				logger.info("Received " + tokens[0].toString());
 				kvActions = new KVActions(p);
 				try {
@@ -110,12 +117,11 @@ public class ECSClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
-		}
-		else if(tokens[0].equals("shutDown")) {
-			if(tokens.length == 1) {
+
+		} else if (tokens[0].equals("shutDown")) {
+			if (tokens.length == 1) {
 				logger.info("Received " + tokens[0].toString());
 				kvActions = new KVActions(p);
 				try {
@@ -123,12 +129,11 @@ public class ECSClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
-		}
-		else if(tokens[0].equals("addNode")) {
-			if(tokens.length == 1) {
+
+		} else if (tokens[0].equals("addNode")) {
+			if (tokens.length == 1) {
 				logger.info("Received " + tokens[0].toString());
 				kvActions = new KVActions(p);
 				try {
@@ -136,12 +141,11 @@ public class ECSClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
-		}
-		else if(tokens[0].equals("remmoveNode")) {
-			if(tokens.length == 1) {
+
+		} else if (tokens[0].equals("remmoveNode")) {
+			if (tokens.length == 1) {
 				logger.info("Received " + tokens[0].toString());
 				kvActions = new KVActions(p);
 				try {
@@ -149,32 +153,28 @@ public class ECSClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
-		}
-		else {
+
+		} else {
 			logger.warn("Received " + tokens[0].toString());
 			printError("Unknown command");
 		}
-		
+
 	}
-	
-	private void printError(String error){
-		System.out.println(PROMPT + "Error! " +  error);
+
+	private void printError(String error) {
+		System.out.println(PROMPT + "Error! " + error);
 	}
-		
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		try {
-			String argument = null ;
+			String argument = null;
 			new LogSetup("logs/ecs/ecs.log", Level.OFF);
-			if (args.length==1) {
-				argument=args[0].toString();
-			}
-			else {
+			if (args.length == 1) {
+				argument = args[0].toString();
+			} else {
 				System.out.println("Wrong input parameters");
 			}
 			ECSClient ecsClient = new ECSClient(argument);
@@ -182,10 +182,7 @@ public class ECSClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-	} 
-	
-	
+
+	}
 
 }
--- ScalableStorageService/src/ecs/KVActions.java
+++ ScalableStorageService/src/ecs/KVActions.java
@@ -1,5 +1,7 @@
 package ecs;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,9 +11,9 @@ import org.apache.log4j.Logger;
 import common.messages.Md5;
 
 public class KVActions implements KVActionsInterface {
-	
+
 	private static Logger logger = Logger.getRootLogger();
-	List <KVServerData> p = new ArrayList<KVServerData>();
+	List<KVServerData> p = new ArrayList<KVServerData>();
 	private Md5 hash = new Md5();
 
 	public KVActions(List<KVServerData> p) {
@@ -19,39 +21,41 @@ public class KVActions implements KVActionsInterface {
 	}
 
 	@Override
-	public void initService(int numberOfNodes) throws Exception {
-		Process proc ;
-		//String script = "../src/app_kvEcs/script.sh";
-		String script = "C:\\Users\\User\\git\\ScalableStorageServer\\ScalableStorageService\\src\\app_kvEcs\\script.sh";
-		System.out.println(script);
+	public void initService(int numberOfNodes, int port) throws Exception {
+		String content = "ssh -n 127.0.0.1 nohup java -jar /Users/roshinisachithanandan/git/ScalableStorageSrvr/ScalableStorageService/ms3-server.jar"
+				+ port + " ERROR &";
+		String script = "/Users/roshinisachithanandan/git/ScalableStorageSrvr/ScalableStorageService/src/app_kvEcs/script.sh";
+		BufferedWriter bw = new BufferedWriter(new FileWriter(script));
+		bw.write(content);
+		bw.flush();
+		bw.close();
+		System.out.println(content);
 		Runtime run = Runtime.getRuntime();
-		for (int i=0 ; i< numberOfNodes ; i ++) {
-			proc = run.exec(script);
+		for (int i = 0; i < numberOfNodes; i++) {
+			run.exec(script);
 			p.get(i).setRunning(true);
-			/*Now send the meta-data list to the remote server */
-			
+			/* Now send the meta-data list to the remote server */
 		}
-		
 	}
 
 	@Override
 	public void start() throws Exception {
-		
-		for (int i=0; i< p.size(); i++) {
+
+		for (int i = 0; i < p.size(); i++) {
 			if (p.get(i).isRunning()) {
 				p.get(i).setAccepting(true);
-				/* Call start function */	
+				/* Call start function */
 			}
 		}
 		/* If successfull then send the meta-data list to every remote server */
-		 
+
 	}
 
 	@Override
 	public void stop() throws Exception {
-		
-		for (int i=0; i< p.size(); i++) {
-			if(p.get(i).isAccepting()) {
+
+		for (int i = 0; i < p.size(); i++) {
+			if (p.get(i).isAccepting()) {
 				p.get(i).setAccepting(true);
 				/* Call stop function */
 			}
@@ -61,63 +65,66 @@ public class KVActions implements KVActionsInterface {
 
 	@Override
 	public void shutDown() throws Exception {
-		
-		for (int i=0; i< p.size(); i++) {
-			if(p.get(i).isRunning()) {
+
+		for (int i = 0; i < p.size(); i++) {
+			if (p.get(i).isRunning()) {
 				p.get(i).setRunning(false);
-				/* Call shutDown function*/
+				/* Call shutDown function */
 			}
-		}	
+		}
 	}
 
 	@Override
 	public void addNode() throws Exception {
-		
-		int counter = 0 ;
+
+		int counter = 0;
 		Random randomGenerator = new Random();
-		boolean newNodeFound = false ;
-		
-		Process proc ;
+		boolean newNodeFound = false;
+
+		Process proc;
 		String script = "./src/app_kvEcs/script.sh";
 		Runtime run = Runtime.getRuntime();
-		
-		
-		for (int i=0; i< p.size(); i++) {
-			if(!p.get(i).isAccepting()) {
-				counter ++ ;
+
+		for (int i = 0; i < p.size(); i++) {
+			if (!p.get(i).isAccepting()) {
+				counter++;
 			}
 		}
-		
-		if (counter!=0) {
+
+		if (counter != 0) {
 			do {
-				int randomInt = randomGenerator.nextInt(p.size()) + 1 ;
-				for (int i=0; i< p.size(); i++) {
-					if((!p.get(i).isAccepting())&&(randomInt==i)) {
-						newNodeFound=true;
+				int randomInt = randomGenerator.nextInt(p.size()) + 1;
+				for (int i = 0; i < p.size(); i++) {
+					if ((!p.get(i).isAccepting()) && (randomInt == i)) {
+						newNodeFound = true;
 						proc = run.exec(script);
 						p.get(i).setRunning(true);
 						p.get(i).setAccepting(true);
-						p.get(i).setPosition(hash.hashIpPort(p.get(i).getIp(),p.get(i).getPort()));
-						/*Initiate server with meta-data and start it using the correct command*/
-						/*Update metadata*/
-						/*Invoke command to update meta-data*/
-						
+						p.get(i).setPosition(
+								hash.hashIpPort(p.get(i).getIp(), p.get(i)
+										.getPort()));
+						/*
+						 * Initiate server with meta-data and start it using the
+						 * correct command
+						 */
+						/* Update metadata */
+						/* Invoke command to update meta-data */
+
 					}
 				}
-				
-			} while (newNodeFound=false);
-			
-		}
-		else {
+
+			} while (newNodeFound = false);
+
+		} else {
 			logger.info("All KVServer instances are accepting connections , there are no new servers to be initiated ");
 		}
-		
+
 	}
 
 	@Override
 	public void removeNode() throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
